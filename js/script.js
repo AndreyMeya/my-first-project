2f69a64a484cdcfb8ebc333c41ce5e9399891e1e
Andrey <amur4ik123@gmail.com>
2021-09-10 10:44:03 +0300

    my first app on waching Movies

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-" use strict";
+'use strict';
 
-let age = prompt('сколько тебе лет?', '');
-alert(`Тебе ${age} лет`);
+// let numberOfFilms = prompt('сколько фильмов вы уже посмотрели?');
+// console.log(numberOfFilms);
+
+// let personalMovieDB = [`count ${numberOfFilms}`];
+// let personalMovieDB =
+
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+   b = prompt('На сколько оцените его?', ''),
+   c = prompt('Один из последних просмотренных фильмов?', ''),
+   d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
